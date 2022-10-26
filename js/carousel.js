39d208fc52c69d9a7200d8ed2a8a0b285b14230c
Melodyhsu88 <melody.yl.hsu@gmail.com>
2022-10-26 13:21:45 +0800

    add

---
+++ js/carousel.js
@@ -0,0 +1,32 @@
+window.addEventListener("load", function(){
+
+    let rowWidth = $('.product-card').width()
+    // console.log(rowWidth)
+    let curIndex = 0;
+
+        
+    //-----------------------------------------btnLeft.onclick
+    $("#btnLeft").click(function(){
+        curIndex--;
+        $('.product-item-row').css("transform","translateX("+rowWidth*curIndex+"px)")
+
+        document.getElementById("btnRight").disabled = false;
+
+        if(curIndex == -6){
+            document.getElementById("btnLeft").disabled = true;
+        }
+    })
+    //-----------------------------------------btnRight.onclick
+    $("#btnRight").click(function(){
+        curIndex++;
+        $('.product-item-row').css("transform","translateX("+rowWidth*curIndex+"px)");
+
+        document.getElementById("btnLeft").disabled = false;
+        if(curIndex == 0){
+            document.getElementById("btnRight").disabled = true;
+        }
+    })
+    document.getElementById()
+
+    });
+
---
+++ js/sidein.js
@@ -0,0 +1,15 @@
+let oth = $(window).innerHeight();
+$(function(){
+    $(window).scroll(function(){
+      let sct = $(this).scrollTop();
+      let all = sct+oth;
+      $(".slideIn").each(function(){
+        let sld = $(this).offset().top;
+        if(all >sld){
+            $(this).addClass("active");
+        }else{
+            $(this).removeClass("active");
+        }
+      });
+    });
+})
